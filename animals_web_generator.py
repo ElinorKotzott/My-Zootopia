7be4c8ce3a98e9bce163953df7d0a9a0f33fb903
Eli_Nor <elinor.kotzott@gmx.de>
2025-02-22 20:41:46 +0100

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -6,29 +6,43 @@ def load_data(file_path):
     return json.load(handle)
 
 animals_data = load_data('animals_data.json')
-print(animals_data)
+#print(animals_data)
 
-
+output = ""
 for animal in animals_data:
-    print()
     name = animal.get('name')
     if name is not None:
-        print("Name: ", name)
+        output = output + f"Name: {name}\n"
 
     diet = animal.get('characteristics').get('diet')
     if diet is not None:
-        print("Diet: ", diet)
+        output = output + f"Diet: {diet}\n"
 
     locations = animal.get('locations')
     if locations is not None and len(locations) != 0:
         location = locations[0]
-        print("Location: ", location)
+        output = output + f"Location: {location}\n"
 
     animal_type = animal.get('characteristics').get('type')
     if animal_type is not None:
-        print("Type: ", animal_type)
+        output = output + f"Type: {animal_type}\n"
+
+print(output)
 
 
 
+try:
+    with open("animals_template.html", "r") as handle:
+        template_data = handle.read()
+except FileNotFoundError:
+    print("The file was not found.")
+
+
+new_data = template_data.replace("__REPLACE_ANIMALS_INFO__", output)
+
+print(new_data)
+
+with open("animals.html", "w") as handle:
+    handle.write(new_data)
 
 
