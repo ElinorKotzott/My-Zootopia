4f01ea51eb23990ce1efd37d45222d3e1eccebd2
Eli_Nor <elinor.kotzott@gmx.de>
2025-02-25 18:11:51 +0100

    added main function, reorganized code, added docstring

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -3,11 +3,15 @@ import json
 def load_data(file_path):
   """ Loads a JSON file """
   with open(file_path, "r") as handle:
-    return json.load(handle)
+    try:
+        return json.load(handle)
+    except FileNotFoundError:
+        print("Cannot load data, file doesn't exist!")
 
-animals_data = load_data('animals_data.json')
+
 
 def serialize_animal(animal_obj):
+    """concatenates strings for the HTML file using the list of dictionaries in animals_data"""
     output = ""
     output = output + "<li class='cards__item'>"
     name = animal_obj.get('name')
@@ -19,51 +23,55 @@ def serialize_animal(animal_obj):
 
     output = output + "<div class='card__text'>\n<ul>\n"
 
-    diet = animal_obj.get('characteristics').get('diet')
-    if diet is not None:
-        output = output + f"<li><strong>Diet:</strong> {diet}</li>\n"
-
     locations = animal_obj.get('locations')
     if locations is not None and len(locations) != 0:
         location = locations[0]
         output = output + f"<li><strong>Location:</strong> {location}</li>\n"
 
-    animal_type = animal_obj.get('characteristics').get('type')
-    if animal_type is not None:
-        output = output + f"<li><strong>Type:</strong> {animal_type}</li>\n"
+    if animal_obj.get('characteristics') is not None:
 
-    lifespan = animal_obj.get('characteristics').get('lifespan')
-    if lifespan is not None:
-        # ensuring the hyphen will be depicted correctly
-        if "–" in lifespan:
-            lifespan = lifespan.replace("–", "-")
-        output = output + f"<li><strong>Lifespan:</strong> {lifespan}</li>\n"
+        diet = animal_obj.get('characteristics').get('diet')
+        if diet is not None:
+            output = output + f"<li><strong>Diet:</strong> {diet}</li>\n"
 
-    lifestyle = animal_obj.get('characteristics').get('lifestyle')
-    if lifestyle is not None:
-        output = output + f"<li><strong>Lifestyle:</strong> {lifestyle}</li>\n"
+        animal_type = animal_obj.get('characteristics').get('type')
+        if animal_type is not None:
+            output = output + f"<li><strong>Type:</strong> {animal_type}</li>\n"
+
+        lifespan = animal_obj.get('characteristics').get('lifespan')
+        if lifespan is not None:
+            # ensuring the hyphen will be depicted correctly
+            if "–" in lifespan:
+                lifespan = lifespan.replace("–", "-")
+            output = output + f"<li><strong>Lifespan:</strong> {lifespan}</li>\n"
+
+        lifestyle = animal_obj.get('characteristics').get('lifestyle')
+        if lifestyle is not None:
+            output = output + f"<li><strong>Lifestyle:</strong> {lifestyle}</li>\n"
 
     output = output + "</ul></div>\n</li>"
     return output
 
-final_output = ""
-for animal in animals_data:
-    final_output = final_output + serialize_animal(animal)
+
+def main():
+    animals_data = load_data('animals_data.json')
+
+    final_output = ""
+    for animal in animals_data:
+        final_output = final_output + serialize_animal(animal)
+    # reads animals_template
+    try:
+        with open("animals_template.html", "r") as handle:
+            template_data = handle.read()
+    except FileNotFoundError:
+        print("The file was not found.")
+    # replaces old string with animal info
+    new_data = template_data.replace("__REPLACE_ANIMALS_INFO__", final_output)
+    # creates new file with HTML and animal info
+    with open("animals.html", "w") as handle:
+        handle.write(new_data)
 
 
-
-#reads animals_template
-try:
-    with open("animals_template.html", "r") as handle:
-        template_data = handle.read()
-except FileNotFoundError:
-    print("The file was not found.")
-
-#replaces old string with animal info
-new_data = template_data.replace("__REPLACE_ANIMALS_INFO__", final_output)
-
-#creates new file with HTML and animal info
-with open("animals.html", "w") as handle:
-    handle.write(new_data)
-
+if __name__ == "__main__":
+    main()
 
