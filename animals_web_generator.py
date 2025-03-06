ec149cbcccc306bc9e194c079a278022cdf95719
Eli_Nor <elinor.kotzott@gmx.de>
2025-03-06 17:00:58 +0100

    added error handling for getting data from API

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -50,18 +50,36 @@ def serialize_animal(animal_obj):
 def main():
     animal = input("Enter an animal name! ")
     url = f"https://api.api-ninjas.com/v1/animals?&name={animal}"
+    try:
+        animals_data = requests.get(url, headers={"X-Api-Key": API_KEY}).json()
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred while fetching the data: {e}")
+        return
 
-    animals_data = requests.get(url, headers={"X-Api-Key": API_KEY}).json()
     if not animals_data:
-        print(f"{animal} doesn't exist!")
+        error_string = f"{animal} doesn't exist!"
+        # reads animals_template
+        try:
+            with open("animals_template.html", "r") as handle:
+                template_data = handle.read()
+        except FileNotFoundError:
+            print("The file was not found.")
+            return None
+        # replaces old string with animal info
+        new_data = template_data.replace("__REPLACE_ANIMALS_INFO__", error_string)
+        # creates new file with HTML and animal info
+        with open("animals.html", "w") as handle:
+            handle.write(new_data)
+        return
     else:
-        print(animals_data)
+        print("Website was successfully generated to the file 'animals.html'.")
 
 
     final_output = ""
     for animal in animals_data:
         final_output = final_output + serialize_animal(animal)
     # reads animals_template
+
     try:
         with open("animals_template.html", "r") as handle:
             template_data = handle.read()
